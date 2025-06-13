74a62d2c3429fb5f72cc6c4c429a9a48d378e2f4
buchi-1996 <itzbuchi@gmail.com>
2025-06-13 16:20:30 +0100

    updates

--- components/usercard/UserCard.tsx
+++ components/usercard/UserCard.tsx
@@ -17,11 +17,11 @@ const UserCard = ({ title, description, imageIcon, href }: UserCardProps) => {
     return (
         <Link href={href} className="w-full min-h-full hover:scale-105 transition-transform duration-300 ease-in-out">
             <Card className="bg-[#ffffff] py-6 shadow-none border cursor-pointer">
-                <CardContent className="flex items-center gap-6">
-                        <Image src={imageIcon} alt="gift icon" width={100} height={100} className="w-8 h-8" />
-                    <div className='flex items-center-safe gap-2'>
+                <CardContent className="flex px-4 md:px-auto items-center gap-4 md:gap-6">
+                        <Image src={imageIcon} alt="gift icon" width={100} height={100} className="w-6 h-6 sm:w-8 sm:h-8" />
+                    <div className='flex-col flex md:flex-row md:items-center gap-2'>
                         <h3 className="text-[1rem] font-semibold text-[#222222]">{title}</h3>
-                        <span>-</span>
+                        <span className="hidden md:block">-</span>
                         <p className="text-[#626262] text-sm">{description}</p>
                     </div>
                 </CardContent>
