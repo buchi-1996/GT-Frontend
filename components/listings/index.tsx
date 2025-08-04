f01dceb2eea2c5aad0083e5683d0a398fb5a1c40
buchi-1996 <itzbuchi@gmail.com>
2025-08-04 10:19:50 +0100

    clean up on matching

--- components/listings/index.tsx
+++ components/listings/index.tsx
@@ -20,17 +20,13 @@ const AddListingItem = () => {
 
     return (
         <div className='flex flex-col min-h-[calc(100vh_-_11rem)] items-center justify-center'>
-            <Image
-                src="/assets/icons/listing-icon.svg"
-                alt="No items found"
-                width={200}
-                height={200}
-                className="mb-4 w-22 h-22"
-            />
-            <div className='grid place-items-center gap-4 mt-2'>
-                <h2 className="text-xl text-app-black font-semibold">No Items Listed</h2>
-                <p className='text-md max-w-sm text-center text-app-base mb-4'>Share items you no longer need with people who can use them.</p>
-                <Button variant="primary" className='w-44 py-6' onClick={() => openAddItem(<ListingForm />)}>Post Item</Button>
+            <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="81" height="80" fill="none"><rect width="79" height="79" x="1" y=".5" fill="#F9F9F9" stroke="#F1F1F1" rx="5.5"/><rect width="37" height="4" x="32.412" y="58.956" fill="#D9D9D9" rx="2"/><rect width="29" height="4" x="32.412" y="66.956" fill="#D9D9D9" rx="2"/><g clipPath="url(#a)"><rect width="16.478" height="16.478" x="11.5" y="55.956" fill="#C9D377" rx="4.119"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="m20.019 60.08-.011 8.75M15.625 64.456h8.75"/></g><rect width="37" height="4" x="32.412" y="10" fill="#D9D9D9" rx="2"/><rect width="29" height="4" x="32.412" y="18" fill="#D9D9D9" rx="2"/><g clipPath="url(#b)"><rect width="16.478" height="16.478" x="11.5" y="7" fill="#C9D377" rx="4.119"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="m20.019 11.125-.011 8.75M15.625 15.5h8.75"/></g><rect width="37" height="4" x="32.412" y="34.478" fill="#D9D9D9" rx="2"/><rect width="29" height="4" x="32.412" y="42.478" fill="#D9D9D9" rx="2"/><g clipPath="url(#c)"><rect width="16.478" height="16.478" x="11.5" y="31.478" fill="#C9D377" rx="4.119"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="m20.019 35.603-.011 8.75M15.625 39.978h8.75"/></g><defs><clipPath id="a"><rect width="16.478" height="16.478" x="11.5" y="55.956" fill="#fff" rx="4.119"/></clipPath><clipPath id="b"><rect width="16.478" height="16.478" x="11.5" y="7" fill="#fff" rx="4.119"/></clipPath><clipPath id="c"><rect width="16.478" height="16.478" x="11.5" y="31.478" fill="#fff" rx="4.119"/></clipPath></defs></svg>
+            </span>
+            <div className='grid place-items-center gap-2 mt-6'>
+                <h2 className="text-lg text-app-black font-semibold">No Items Listed</h2>
+                <p className='w-full text-sm md:text-md max-w-xs text-center text-app-base mb-4'>Share items you no longer need with people who can use them.</p>
+                <Button variant="primary" className='w-auto md:w-44 py-6' onClick={() => openAddItem(<ListingForm />)}>Post Item</Button>
             </div>
         </div>
     )
