426cc96322266d42ae70860b367a1d10d0279075
buchi-1996 <itzbuchi@gmail.com>
2025-11-23 09:13:25 +0100

    feat/Receiver_pickup

--- components/header/User.tsx
+++ components/header/User.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
 const User = () => {
     return (
-        <Link href="/dashboard/settings/profile" className="flex items-center gap-3 hover:bg-gray-100 p-2 rounded-md transition-colors duration-200">
+        <Link href="#" className="flex items-center gap-3 hover:bg-gray-100 p-2 rounded-md transition-colors duration-200">
             <div className={`flex items-center gap-3 `}>
             <Avatar className="w-10 h-10 sm:w-8 sm:h-8">
                 <AvatarImage src="/placeholder.svg?height=32&width=32" />
